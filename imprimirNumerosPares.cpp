57e53680e287f63dcc32e33f4ec79b0bf1a35fcd
atarico <atarico@gmail.com>
2024-04-17 12:19:27 -0300

    prueba

---
+++ imprimirNumerosPares.cpp
@@ -0,0 +1,22 @@
+/*
+    CICLO
+    IMPRIMIR LOS NUMEROS PARES
+*/
+
+/// ESCRIBIR UN PROGRAMA QUE LE PIDA UN NUMERO ENTERO "N" AL USUARIO
+/// Y QUE IMPRIMA TODOS NUMEROS PARES HASTA ESE "N"
+
+int numero = 0;
+int i = 1;
+
+while (i <= numero){
+    ///magia
+
+    i+=1;
+}
+
+
+
+for (int i = 1; i <= numero; i++){
+    ///magia
+}
